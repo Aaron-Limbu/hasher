e47e738fdeccc5d426dee837934b5b757415bad8
Escalon <134317633+Aaron-Limbu@users.noreply.github.com>
2024-06-15 19:26:01 +0545

    Update hasher.py

--- hasher.py
+++ hasher.py
@@ -17,25 +17,28 @@ def prompt_user_input():
     return uinput 
 
 def hashfile(file, method):
-    B_size = 66666
-    if method == 1: 
-        hasher = hashlib.sha512()
-    elif method == 2: 
-        hasher = hashlib.md5()
-    elif method ==3 : 
-        hasher = hashlib.sha224()
-    elif method == 4: 
-        hasher = hashlib.sha3_224()
-    elif method == 5: 
-        hasher = hashlib.sha3_512()
-    else:
-        raise ValueError("Invalid method. Use 1 for SHA-512 and 2 for MD5.")
+    try: 
+        B_size = 66666
+        if method == 1: 
+            hasher = hashlib.sha512()
+        elif method == 2: 
+            hasher = hashlib.md5()
+        elif method ==3 : 
+            hasher = hashlib.sha224()
+        elif method == 4: 
+            hasher = hashlib.sha3_224()
+        elif method == 5: 
+            hasher = hashlib.sha3_512()
+        else:
+            raise ValueError("Invalid method. Use 1 for SHA-512 and 2 for MD5.")
 
-    with open(file, "r", encoding="ISO-8859-1") as rfile:
-        for line in rfile:
-            strings.append(line)
-            hasher.update(line.encode('ISO-8859-1'))
-            nstrings.append(hasher.hexdigest())
+        with open(file, "r", encoding="ISO-8859-1") as rfile:
+            for line in rfile:
+                strings.append(line)
+                hasher.update(line.encode('ISO-8859-1'))
+                nstrings.append(hasher.hexdigest())
+    except Exception as e : 
+        print(f'{e}')
                 
 def write_output(output_file):
     if output_file:
@@ -47,17 +50,21 @@ def write_output(output_file):
             print(hash_string)
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Hashes your text file\n")
-    parser.add_argument('-i', '--input', type=str, help='Input the file, example: a.txt\n')
-    parser.add_argument('-o', '--output', type=str, help='Output file name\n')
-    parser.add_argument('-m', '--method', type=int, help='Hash method: 1 for SHA-512, 2 for MD5\n', required=True)
-    args = parser.parse_args()
-    
-    if args.input:
-        in_file = args.input
-    else:
-        in_file = prompt_user_input()
-        
-    hashfile(in_file, args.method)
-    
-    write_output(args.output)
+    try: 
+        parser = argparse.ArgumentParser(description="Hashes your text file\n")
+        parser.add_argument('-i', '--input', type=str, help='Input the file, example: a.txt\n')
+        parser.add_argument('-o', '--output', type=str, help='Output file name\n')
+        parser.add_argument('-m', '--method', type=int, help='Hash method: 1 for SHA-512, 2 for MD5\n', required=True)
+        args = parser.parse_args()
+        if args.input:
+            in_file = args.input
+        else:
+            in_file = prompt_user_input()  
+        hashfile(in_file, args.method)
+        write_output(args.output)
+    except FileNotFoundError as e: 
+        print(f'{e}')
+    except KeyboardInterrupt as e: 
+        print(f'{e}')
+    except Exception as e : 
+        print(f'{e}')
